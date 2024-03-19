5322f4eb789202829577002a2a7aca95bafdb9fc
acretph_jamanus <jamaica.angely.manus@acret-ph.com>
2024-03-19 12:42:44 +0800

    update hash and secret key

--- website/__init__.py
+++ website/__init__.py
@@ -9,7 +9,7 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SECRET_KEY'] = 'asdfghjkl12345'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
