18a834fa5ea991a852d7ea49dcbcb8e6c277b202
acretph_jamanus <jamaica.angely.manus@acret-ph.com>
2024-03-20 10:37:22 +0800

    Update home page display and create filter by category page

--- website/views.py
+++ website/views.py
@@ -23,7 +23,13 @@ def home():
         name_url = f"https://www.thecocktaildb.com/api/json/v1/1/search.php?s={search_query}"
         drinks = get_json_data(name_url).get('drinks', [])
         return render_template("search-cocktail.html", user=current_user, drinks=drinks)
-    return render_template("home.html", user=current_user)
+
+    api_lnk = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=ice"
+    random_drinks = get_json_data(api_lnk).get('drinks', [])
+
+    api_cat = "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list"
+    bycat = get_json_data(api_cat).get('drinks', [])
+    return render_template("home.html", user=current_user, random_drinks=random_drinks, cates=bycat)
 
 # Function to handle search by letter route
 @views.route('/browse/letter/<lett>', methods=['GET', 'POST'])
@@ -31,6 +37,17 @@ def searchby_letter(lett):
     ltr_url = f"https://www.thecocktaildb.com/api/json/v1/1/search.php?f={lett}"
     ltr_data = get_json_data(ltr_url).get('drinks', [])
     return render_template("search-by-letter.html", user=current_user, letter=ltr_data)
+
+# Function to handle filter by category route
+@views.route('/browse/category/<cat>', methods=['GET', 'POST'])
+def filterby_category(cat):
+    cat_api = f"https://www.thecocktaildb.com/api/json/v1/1/filter.php?c={cat}"
+    getcat = get_json_data(cat_api).get('drinks', [])
+
+    # get category list
+    api_cat = "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list"
+    bycat = get_json_data(api_cat).get('drinks', [])
+    return render_template("filter-by-category.html", user=current_user, categories=getcat, cates=bycat)
 
 # Function to handle drink detail route
 @views.route('/drink/<did>', methods=['GET', 'POST'])
